bda0ef1071b925942fdb9cd431599fb35858b637
BUYRAK <hello@tuxisoft.com>
2022-11-23 21:20:47 +0100

    Added Lab02-p01

---
+++ lab02/part01.py
@@ -0,0 +1,35 @@
+# 02.1.1 Two numbers.
+"""
+numberOne: int = 5
+numberTwo: int = 10
+
+print('Sum of two integers variable: ' + str(numberOne+numberTwo))
+print('Diffrence of two integers variable: ' + str(numberTwo-numberOne))
+print('Average of two integers variable: ' + str(numberTwo+numberOne/2))
+print('Value of the difference two integers variable: ' + str(abs(numberTwo+numberOne)))
+print('Most max veriable: ' + str(max(numberTwo, numberOne)))
+print(f"Most min veriable: {str(min(numberTwo, numberOne))}")
+"""
+# 02.1.2 Resistances.
+"""
+resistanceOne = "Snake"
+resistanceTwo = "Chicken"
+resistanceThree = "Goat"
+print(f"{resistanceOne[:3]}...{resistanceOne[3:]}")
+print(f"{resistanceTwo[:3]}...{resistanceTwo[3:]}")
+print(f"{resistanceThree[:3]}...{resistanceThree[3:]}")
+"""
+# 02.1.3 Digits.
+"""
+print(f"Please enter 5 digits number:")
+positiveNumber = input()
+stringExpression = str(positiveNumber)
+
+print(f"First number:{stringExpression[:1]} \n"
+      f"Second number:{stringExpression[1:2]} \n"
+      f"Third number:{stringExpression[2:3]} \n"
+      f"Fourth number:{stringExpression[3:4]} \n"
+      f"Fifth number:{stringExpression[4:5]} \n")
+"""
+# 02.1.4 Hybrid car.
+
