bdd93532cd7f4e2d147f8dba282452d7aaa6c5f4
BUYRAK <hello@tuxisoft.com>
2022-11-27 00:06:50 +0100

    Lab03 part01 is finished

--- lab03/part01.py
+++ lab03/part01.py
@@ -85,4 +85,41 @@ if x == 3.0:
 print(s)
 """
 # 03.1.5 Matter of logic.
+"""
+valueNumber = int(input("Please enter a value: "))
+
+if valueNumber >= 0:
+    print("Entered value is big than 0 or the value is same")
+elif valueNumber <= 0:
+    print("Entered value is small than 0 or the value is same")
+elif 0 < valueNumber <= 100:
+    print("The value is big than 0 and small than 100")
+elif valueNumber == 0 or valueNumber >= 200 and len(valueNumber) == 3:
+    print("Entered value is big than 200 or the value is 0")
+"""
+# 03.1.6 De Morgan.
+"""
+x = int(input("Insert an integer number: "))
+
+ruleOne = not (0 < x < 100)
+ruleTwo = x <= 0 or x >= 100
+print('not (0 < x < 100', ruleOne)
+print('x <= 0 or x >= 100', ruleTwo)
+
+ruleThree = not (x > 0 or x < 100)
+ruleFour = 0 >= x >= 100
+print('not (x > 0 or x < 100)', ruleThree)
+print('x >= 0 and x >= 100', ruleFour)
+
+ruleFive = not (x > 0 or 100 < x)
+ruleSix = 0 > x > 100
+print('not (x > 0 or 100 < x)', ruleFive)
+print('0 > x > 100', ruleSix)
+
+ruleSeven = not (0 < x < 100 or x == -1)
+ruleEight = 0 < x < 100 or x != -1
+print('not (x > 0 or 100 < x)', ruleSeven)
+print('x <= 0 and x >= 100 or x != -1', ruleEight)
+"""
+
 
