25460cfaed9e01e1e87c86a5f38b6567d44ba01d
BUYRAK <siyahmerkez@gmail.com>
2022-11-29 00:58:28 +0100

    Lab04 part01 is updated

--- lab04/part01.py
+++ lab04/part01.py
@@ -1,3 +1,25 @@
 # 04.1.1 Integer numbers.
+"""
+numOfNumber = int(input("Please enter a number for numbers: "))
+x = 0
+numbers = []
+subNumbers = []
+while x < numOfNumber:
+    number = int(input("Please enter a number: "))
+    if x > 0:
+        subNumber = numbers[-1] + number
+        subNumbers.append(subNumber)
+    numbers.append(number)
+    x += 1
 
-number = [1,2,3]
+print(numbers)
+y = 0
+while y < numOfNumber-1:
+    if y != len(numbers):
+        firstValue = numbers[y]
+        y += 1
+        secondValue = numbers[y]
+        sumTotal = firstValue + secondValue
+        print(sumTotal)
+"""
+# 04.1.2 String analysis.
