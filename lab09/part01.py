c480179d91183df6e52689860a104a4f56dc0fbb
BUYRAK <siyahmerkez@gmail.com>
2022-12-18 18:23:36 +0100

    lab09 part01 is updated.

--- lab09/part01.py
+++ lab09/part01.py
@@ -123,11 +123,6 @@ def main():
     """
     # Part 1.2/6
     """
-    l = []
-
-    for i in range(1, 11):
-        l.append(i ** 2)
-
     l = [0]
     for i in range(1, 10):
         if l[-1] == 0:
@@ -182,6 +177,33 @@ def main():
         x += 1
     """
     # Part 1.6
+    """
+    """
+    # Part 1.7
+    """
+    """
+    # Part 1.8
+    """
+    players_count = int(input("Enter players count: "))
+    players = []
+    word_list = []
+    guess = ""
+    player = 0
+    for i in range(players_count):
+        players.append(player)
+        if len(word_list) == 0:
+            word = str(input("Enter a word: "))
+            word_list.append(word)
+        guess = input(f"Player {player} \"{word[0:3]}\" Guess the word: ")
+        player += 1
+        if guess == word:
+            word_list[-1] = guess
+
+    if guess == word_list[-1]:
+        print(f"Success User {player}")
+    """
+
+
 
 
 if __name__ == "__main__":
