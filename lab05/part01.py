07099559c6583c2a847c13c510c1dff444075ef9
buyrak <siyahmerkez@gmail.com>
2022-12-25 04:01:05 +0100

    lab05 part01 is added.

---
+++ lab05/part01.py
@@ -0,0 +1,24 @@
+def main():
+    # 1.1
+    """
+    cardPin = "1234"
+    enterPin = input("Please enter your Pin: ")
+    pin_attempts = 1
+
+    while pin_attempts < 3:
+        if enterPin == cardPin:
+            print("Success your pin is correct!")
+            break
+        else:
+            pin_attempts += 1
+            enterPin = input(f"Please enter again your Pin (Your Attempts: {pin_attempts}): ")
+        if pin_attempts == 3:
+            print("Your card is blocked.")
+    """
+    # 1.2
+
+
+
+
+if __name__ == '__main__':
+    main()
